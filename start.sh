c8a1a94ddb34a39e069f596e0048d2386518fec2
SovTek <houjen.huang@yahoo.com>
2026-05-17 17:43:23 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 sed -i "s/listen 8080;/listen ${PORT:-8080};/" /etc/nginx/sites-available/default
-python3 -m wisp.server --port 8081 &
+wisp-server --port 8081 --host 127.0.0.1 &
 gunicorn --bind 127.0.0.1:5000 app:app &
 nginx -g "daemon off;"
