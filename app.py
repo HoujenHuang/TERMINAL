d0c619db07f177aa2d24f6423344899d49bdb191
SovTek <houjen.huang@yahoo.com>
2026-05-24 16:50:10 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,30 +1,11 @@
 import os
-from flask import Flask, request, jsonify
-from flask_cors import CORS
+from flask import Flask, jsonify
 
 app = Flask(__name__)
-CORS(app)
-
-latest_metrics = {"cpu_usage": 0}
 
 @app.route('/')
-def index():
-	return jsonify({"message": "TERMINAL Backend is running"}), 200
-
-@app.route('/report', methods=['POST'])
-def report():
-	data = request.json
-	latest_metrics['cpu_usage'] = data.get('cpu_usage', 0)
-	return jsonify({"status": "success"}), 200
-
-@app.route('/metrics', methods=['GET'])
-def get_metrics():
-	return jsonify(latest_metrics), 200
+def home():
+	return jsonify({"message": "TERMINAL Backend is running"})
 
 if __name__ == "__main__":
-	app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
-
-@app.route('/favicon.ico')
-def favicon():
-	return send_from_directory(os.path.join(app.root_path, 'static'),
-								'favicon.svg', mimetype='image/svg+xml')
+	app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
