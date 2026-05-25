bc343cb495d4b4b4bed24046d80e1c69e3e14e5e
SovTek <houjen.huang@yahoo.com>
2026-05-25 19:16:38 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,26 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import os
-from flask import Flask, jsonify
 
-app = Flask(__name__)
+app = FastAPI()
 
-@app.route('/')
-def home():
-	return jsonify({"message": "TERMINAL Backend is running"})
+app.add_middleware(
+	CORSMiddleware,
+	allow_origins=["*"],
+	allow_credentials=True,
+	allow_methods=["*"],
+	allow_headers=["*"],
+)
+
+@app.get("/")
+def read_root():
+	return {"status": "TERMINAL Backend is running. Open /api/data to get info."}
+
+@app.get("/api/data")
+def get_data():
+	return {"message": "TERMINAL Backend is running.", "data: ": [1, 2, 3, 4, 5]}
 
 if __name__ == "__main__":
-	app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+	import uvicorn
+	port = int(os.environ.get("PORT", 8080))
+	uvicorn.run(app, host="0.0.0.0", port=port)
