7bd6beebe121d0597baaa22b5f5daa847fb25eaa
SovTek <houjen.huang@yahoo.com>
2026-06-21 18:41:36 -0400

    Create config.js

---
+++ config.js
@@ -0,0 +1,15 @@
+const CONFIG = {
+	YOUTUBE_API_KEY: "AIzaSyARfwKry_c26u7Y86Tc1nUE1tkTDv-BS3Y",
+
+	ADMINS: {
+		"houjen@terminal": {
+			password: "а ну чики брики и в дамки"
+		},
+		"dylan@terminal": {
+			password: "BorschtTheBear"
+		},
+		"rithvik@terminal": {
+			password: "రిత్విక్_గుమ్మ"
+		}
+	}
+};
